0ddb56ad660ba1c74cc5ee614eae68600fec8f94
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-03-26 14:22:52 +0100

    Pulpit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { AppstoreOutlined, CalendarOutlined , CalculatorOutlined, UserOutlined, SettingOutlined, BuildOutlined } from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
 
-function App() {
+const { Header, Content, Sider } = Layout;
 
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/')
-      .then(response => response.text())
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  return (
-    <div>
-      <h1>Server Data:</h1>
-      <p>{data || 'No data yet'}</p>
-    </div>
-  );
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
 }
+
+const items = [
+  getItem('Pulpit', '1', <AppstoreOutlined />),
+  getItem('Kalendarz', '2', <CalendarOutlined />),
+  getItem('Zadania', '3', <CalculatorOutlined />),
+  getItem('Członkowie', '4', <UserOutlined />),
+  getItem('Ustawienia', '5', <SettingOutlined />),
+  { type: 'divider' },
+  getItem('Moje projekty', 'grp', null, [
+    getItem('Projekt 1', '11'), getItem('Projekt 2', '12')
+  ], 'group'),
+];
+
+const App = () => {
+  const [currentPage, setCurrentPage] = React.useState('Pulpit');
+
+  const onClick = (e) => {
+    console.log('click ', e);
+  };
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider width={256} className="sider">
+      <div className="logo">
+          <BuildOutlined style={{ color: '#5503da', fontSize: '32px', marginRight: '16px' }} />
+          <span className="app-name">Taskify</span>
+        </div>
+        <Menu
+          onClick={onClick}
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="header" />
+        <Content className="content">{currentPage}</Content>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default App;
