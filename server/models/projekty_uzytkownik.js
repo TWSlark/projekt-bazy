8602e2f62d4b674cf4f6d27ee24a349fd3a6cc1d
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-04-29 21:45:36 +0200

    poprawka tabeli pomocniczej

--- server/models/projekty_uzytkownik.js
+++ server/models/projekty_uzytkownik.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
     const ProjektyUzytkownik = sequelize.define('projekty_uzytkownik', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
         projekt_id: {
             type: DataTypes.INTEGER,
             allowNull: false
